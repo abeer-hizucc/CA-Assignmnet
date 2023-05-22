e18c1b40e08047a5b55bd435d8b4c9f0f72e7722
abeer-hizucc <marksmanabr@gmail.com>
2023-05-22 16:11:32 +0600

    loginComponent finished and path is initialised

--- Email-Verification/src/app/login-form/login-form.component.ts
+++ Email-Verification/src/app/login-form/login-form.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent {
-
+export class LoginFormComponent implements OnInit {
+login :FormGroup|any;
+  constructor() { }
+  ngOnInit(): void {
+  }
+  logindata(login:FormGroup){
+    console.log(login.value)
+  }
 }
