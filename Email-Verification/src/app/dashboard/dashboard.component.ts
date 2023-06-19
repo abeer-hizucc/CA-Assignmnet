0039cefc5328f523cacea4a017636c2d28e2d1c5
abeer-hizucc <marksmanabr@gmail.com>
2023-06-19 11:17:10 +0600

    logout button added

--- Email-Verification/src/app/dashboard/dashboard.component.ts
+++ Email-Verification/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GoogleApiService } from '../services/google-api.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  logoutToken!:Subscription;
+  constructor(private googleLogOut: GoogleApiService) { }
+  ngOnInit(): void {
+    
+  }
+  logout() {
+    this.googleLogOut.logout();
+    console.log('Logged out from Google');
+  }
+
 
 }
