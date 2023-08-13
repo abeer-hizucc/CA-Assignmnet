3f4cf5d897aaf7f7a33a6311d2b188412d65bdbe
abeer-hizucc <marksmanabr@gmail.com>
2023-08-13 16:18:10 +0600

    toastr added

--- Email-Verification/src/app/services/1stform.service.ts
+++ Email-Verification/src/app/services/1stform.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { catchError, throwError } from "rxjs";
 import { BackendResponse } from "../models/FormBackendResponse.model";
+import { ToastrService } from "ngx-toastr";
 @Injectable({
     providedIn: 'root'
   })
   export class FormService {
     isSubmitting!: boolean;
   
-    constructor(private http: HttpClient, private router: Router) { }
+    constructor(private http: HttpClient, private router: Router, private toastr:ToastrService) { }
   
     submitForm(formData: any): void {
       this.http.post<BackendResponse>('http://localhost:9090/form-fillup', formData)
@@ -24,6 +25,7 @@ import { BackendResponse } from "../models/FormBackendResponse.model";
   
           if (response.success) {
             console.log('Success message:', response.message);
+            this.toastr.success('Form submitted successfully', 'Success!');
             this.router.navigate(['/2ndeKYCForm']);
           }
         }, error => {
